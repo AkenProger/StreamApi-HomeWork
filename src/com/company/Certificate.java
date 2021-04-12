e4c6e64e35616a70013b9fcc3d7fc2497ae70506
AkenProger <meerbek11@mail.ru>
2021-04-12 08:53:10 +0300

    Home Work limit, distintm peek,skip

---
+++ src/com/company/Certificate.java
@@ -0,0 +1,6 @@
+package com.company;
+
+public enum Certificate {
+    CERTIFICATED,
+    NO_CERTIFICATED
+}
--- src/com/company/SeedlingsInformation.java
+++ src/com/company/SeedlingsInformation.java
@@ -1,22 +1,25 @@
 package com.company;
 
 import java.util.List;
+import java.util.Objects;
 
-public class SeedlingsInformation {
+public class SeedlingsInformation implements Comparable {
     private String seedlingName;
     private int seedlingLifeTime;
     private SeedlingTypes seedlingTypes;
     private BirthplaceOfSeedling birthplaceOfSeedling;
     private double price;
     private List<String> comments;
+    private Certificate certificate;
 
-    public SeedlingsInformation(String seedlingName, int seedlingLifeTime, SeedlingTypes seedlingTypes, BirthplaceOfSeedling birthplaceOfSeedling, double price,List<String> comments) {
+    public SeedlingsInformation(String seedlingName, int seedlingLifeTime, SeedlingTypes seedlingTypes, BirthplaceOfSeedling birthplaceOfSeedling, double price,List<String> comments,Certificate certificate) {
         this.seedlingName = seedlingName;
         this.seedlingLifeTime = seedlingLifeTime;
         this.seedlingTypes = seedlingTypes;
         this.birthplaceOfSeedling = birthplaceOfSeedling;
         this.price = price;
         this.comments = comments;
+        this.certificate = certificate;
     }
 
     public String getSeedlingName() {
@@ -68,6 +71,33 @@ public class SeedlingsInformation {
         this.comments = comments;
     }
 
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SeedlingsInformation that = (SeedlingsInformation) o;
+        return seedlingLifeTime == that.seedlingLifeTime &&
+                Double.compare(that.price, price) == 0 &&
+                Objects.equals(seedlingName, that.seedlingName) &&
+                seedlingTypes == that.seedlingTypes &&
+                birthplaceOfSeedling == that.birthplaceOfSeedling &&
+                Objects.equals(comments, that.comments);
+    }
+
+    public Certificate getCertificate() {
+        return certificate;
+    }
+
+    public void setCertificate(Certificate certificate) {
+        this.certificate = certificate;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(seedlingName, seedlingLifeTime, seedlingTypes, birthplaceOfSeedling, price, comments);
+    }
+
     @Override
     public String toString() {
         return "SeedlingsInformation{" +
@@ -76,6 +106,13 @@ public class SeedlingsInformation {
                 ", seedlingTypes=" + seedlingTypes +
                 ", birthplaceOfSeedling=" + birthplaceOfSeedling +
                 ", price=" + price +
+                ", comments=" + comments +
+                ", certificate=" + certificate +
                 '}';
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        return 0;
     }
 }
